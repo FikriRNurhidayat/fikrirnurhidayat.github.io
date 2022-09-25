20b838016ed7b550464f889fbac8d630b76153f2
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2022-09-25 22:50:49 +0700

    feat: revamp design on blog

--- assets/html/javascript-dasar/basic-syntax-error.js
+++
@@ -1,5 +0,0 @@
-const name = "Sabrina";
-const ꦲꦱ꧀ꦩ = "Sabrina";
-
-console.log(Name); // Ini pasti error!
-console.log(ꦲꦱ꧀ꦩ);
--- config/build.el
+++ config/build.el
@@ -2,13 +2,13 @@
 ;;
 ;; Copyright (C) 2021 Fikri Rahmat Nurhidayat
 ;;
-;; Author: Fikri Rahmat Nurhidayat <https://github.com/fain>
+;; Author: Fikri Rahmat Nurhidayat <https://github.com/fikrirnurhidayat>
 ;; Maintainer: Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
 ;; Created: December 20, 2021
 ;; Modified: December 20, 2021
 ;; Version: 0.0.1
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/fain/build-config
+;; Homepage: https://github.com/fikrirnurhidayat/fikrirnurhidayat.github.io
 ;; Package-Requires: ((emacs "24.3"))
 ;;
 ;; This file is not part of GNU Emacs.
@@ -60,9 +60,8 @@
              :recursive t
              :base-directory "./content"
              :publishing-directory "./dist"
-             :auto-sitemap t
              :with-author nil
-             :with-toc t
+             :with-toc nil
              :with-timestamps t
              :section-numbers nil
              :publishing-function org-html-publish-to-html)
